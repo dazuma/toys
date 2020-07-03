64ed4d07ab973115c6f392ebe989d987ff0a474d
Daniel Azuma <dazuma@gmail.com>
2020-07-03 11:32:28 -0700

    Testing

--- .toys/test.rb
+++ .toys/test.rb
@@ -6,10 +6,12 @@ include :terminal
 include :exec
 
 def handle_gem(gem_name)
-  $stderr.puts "to stderr"
   puts("**** Testing #{gem_name}...", :bold, :cyan)
   ::Dir.chdir(::File.join(context_directory, gem_name)) do
+    $stderr.puts "executing"
     result = exec_separate_tool("test")
+    $stderr.puts result.status.inspect
+    $stderr.puts result.exception.inspect
     exit(result.exit_code) unless result.success?
   end
 end
