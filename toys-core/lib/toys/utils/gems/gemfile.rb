8e3426502b6e9ebbf413222002204216087156d4
Daniel Azuma <dazuma@gmail.com>
2020-07-09 21:51:39 -0700

    Fix a warning

--- toys-core/lib/toys/utils/gems/gemfile.rb
+++ toys-core/lib/toys/utils/gems/gemfile.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-unless @__toys_dev_gemspec__
+unless defined?(@__toys_dev_gemspec__)
   gem("toys-core", ::Toys::Core::VERSION)
   gem("toys", ::Toys::VERSION) if ::Toys.const_defined?(:VERSION)
 end
