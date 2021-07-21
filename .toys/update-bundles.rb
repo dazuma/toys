a5337e06660969e5ba68256dd7e84bd7c72f1b48
Daniel Azuma <dazuma@google.com>
2021-07-20 20:27:09 -0700

    chore: Omit vendored gems in test directories from rubocop (#134)

--- .toys/update-bundles.rb
+++ .toys/update-bundles.rb
@@ -5,14 +5,16 @@ desc "Update bundles in both gems"
 include :terminal
 include :exec, exit_on_nonzero_status: true
 
-def handle_gem(gem_name)
-  puts("**** Updating #{gem_name} bundle...", :bold, :cyan)
-  ::Dir.chdir(::File.join(context_directory, gem_name)) do
-    exec(["bundle", "update"])
-  end
-end
-
 def run
-  handle_gem("toys-core")
-  handle_gem("toys")
+  ::Dir.chdir(context_directory)
+
+  puts("**** Updating root bundle...", :bold, :cyan)
+  exec(["bundle", "update"])
+
+  ["toys-core", "toys"].each do |gem_name|
+    puts("**** Updating #{gem_name} bundle...", :bold, :cyan)
+    ::Dir.chdir(gem_name) do
+      exec(["bundle", "update"])
+    end
+  end
 end
