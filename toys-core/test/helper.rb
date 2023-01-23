26101be307be7c9dbfd0c54f0afbec8bcea94e64
Daniel Azuma <dazuma@gmail.com>
2023-01-23 10:32:08 -0800

    fix: Fixed missing require when utils/xdg or utils/git_cache is required in isolation (#202)

--- toys-core/test/helper.rb
+++ toys-core/test/helper.rb
@@ -4,3 +4,17 @@ require "minitest/autorun"
 require "minitest/focus"
 require "minitest/rg"
 require "toys-core"
+require "toys/utils/exec"
+
+module Toys
+  module TestHelper
+    def isolate_ruby
+      lib_path = ::File.join(::File.dirname(__dir__), "lib")
+      executor = Toys::Utils::Exec.new
+      executor.exec_ruby([], in: :controller) do |controller|
+        controller.in.puts "$LOAD_PATH.unshift(#{lib_path.inspect})"
+        yield controller.in
+      end
+    end
+  end
+end
