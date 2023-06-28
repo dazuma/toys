d86d4ff63a02d038350a2aff8be2d13da1b6d030
Daniel Azuma <dazuma@google.com>
2023-06-28 23:40:59 +0000

    chore: Minor fixes to documentation

--- toys-core/lib/toys/standard_mixins/git_cache.rb
+++ toys-core/lib/toys/standard_mixins/git_cache.rb
@@ -8,16 +8,16 @@ module Toys
     # This mixin provides an instance of {Toys::Utils::GitCache}, providing
     # cached access to files from a remote git repo.
     #
-    # Example usage:
+    # @example
     #
-    #     include :git_cache
+    #   include :git_cache
     #
-    #     def run
-    #       # Pull and cache the HEAD commit from the Toys repo.
-    #       dir = git_cache.find("https://github.com/dazuma/toys.git")
-    #       # Display the contents of the readme file.
-    #       puts File.read(File.join(dir, "README.md"))
-    #     end
+    #   def run
+    #     # Pull and cache the HEAD commit from the Toys repo.
+    #     dir = git_cache.get("https://github.com/dazuma/toys.git")
+    #     # Display the contents of the readme file.
+    #     puts File.read(File.join(dir, "README.md"))
+    #   end
     #
     module GitCache
       include Mixin
