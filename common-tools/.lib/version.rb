3351caf05d37c78b4e5115269a292c4ee8a0d581
Daniel Azuma <dazuma@gmail.com>
2025-03-02 20:22:12 -0800

    feat: Release scripts support non-gem releasable units
    feat: Release scripts support more flexible coordination grouping

---
+++ common-tools/.lib/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Toys
+  module CommonTools
+    ##
+    # Current version of the Toys common tools.
+    # @return [String]
+    #
+    VERSION = "0.15.5.1"
+  end
+end
