77109c9c2936ee06b87600422d90505748fe5a13
Daniel Azuma <dazuma@gmail.com>
2025-11-05 14:43:50 -0800

    feat(core): Support for specifying installation options on individual gem installs via the gems mixin

--- toys-core/test/mixins/test_gems.rb
+++ toys-core/test/mixins/test_gems.rb
@@ -14,6 +14,7 @@ describe Toys::StandardMixins::Gems do
   let(:cli) {
     Toys::CLI.new(executable_name: executable_name, logger: logger, middleware_stack: [])
   }
+  let(:exec_service) { Toys::Utils::Exec.new }
 
   it "provides a gems instance" do
     test = self
@@ -23,6 +24,24 @@ describe Toys::StandardMixins::Gems do
         test.assert_instance_of(Toys::Utils::Gems, gems)
         def run
           exit(gems.is_a?(Toys::Utils::Gems) ? 1 : 2)
+        end
+      end
+    end
+    assert_equal(1, cli.run("foo"))
+  end
+
+  it "passes arguments to gems util constructor" do
+    exec_service.exec(["gem", "uninstall", "highline", "--version=2.0.1"], out: :null)
+    cli.add_config_block do
+      tool "foo" do
+        include :gems
+        def run
+          begin
+            gem("highline", "2.0.1", on_missing: :error)
+          rescue Toys::Utils::Gems::ActivationFailedError
+            exit(1)
+          end
+          exit(2)
         end
       end
     end
